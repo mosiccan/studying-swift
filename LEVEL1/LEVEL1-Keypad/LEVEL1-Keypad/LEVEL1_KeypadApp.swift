7b739236587c3ce766ea4ddcc82c691bf13dc964
WooWon Kang (Mac) <mosiccan@gmail.com>
2023-01-10 14:33:40 +0900

    feat : LEVEL1 Keypad 생성

---
+++ LEVEL1/LEVEL1-Keypad/LEVEL1-Keypad/LEVEL1_KeypadApp.swift
@@ -0,0 +1,17 @@
+//
+//  LEVEL1_KeypadApp.swift
+//  LEVEL1-Keypad
+//
+//  Created by 강우원 on 2023/01/10.
+//
+
+import SwiftUI
+
+@main
+struct LEVEL1_KeypadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
