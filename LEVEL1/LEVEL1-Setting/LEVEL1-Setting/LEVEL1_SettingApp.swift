4ba46a63c11ff27f1e91736857b4ad8f64b8618a
WooWon Kang (Mac) <mosiccan@gmail.com>
2023-01-27 14:34:45 +0900

    feat : LEVEL1 Setting 생성

---
+++ LEVEL1/LEVEL1-Setting/LEVEL1-Setting/LEVEL1_SettingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LEVEL1_SettingApp.swift
+//  LEVEL1-Setting
+//
+//  Created by 강우원 on 2023/01/27.
+//
+
+import SwiftUI
+
+@main
+struct LEVEL1_SettingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LEVEL1/LEVEL1-Setting/LEVEL1-SettingUITests/LEVEL1_SettingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LEVEL1_SettingUITestsLaunchTests.swift
+//  LEVEL1-SettingUITests
+//
+//  Created by 강우원 on 2023/01/27.
+//
+
+import XCTest
+
+final class LEVEL1_SettingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
