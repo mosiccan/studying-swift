7b7c5c7fef62f11e585593499a52050057f523ff
WooWon Kang (Mac) <mosiccan@gmail.com>
2023-02-12 14:59:40 +0900

    feat : LEVEL3 Weather 생성

---
+++ LEVEL3/LEVEL3-Weather/LEVEL3-Weather/LEVEL3_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LEVEL3_WeatherApp.swift
+//  LEVEL3-Weather
+//
+//  Created by 강우원 on 2023/02/12.
+//
+
+import SwiftUI
+
+@main
+struct LEVEL3_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
